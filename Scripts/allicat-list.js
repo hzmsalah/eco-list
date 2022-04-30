53b41b0d5119302bef6247edb04e17e3abeb7bfe
Hazem Mahmoud <96292005+hzmsalah@users.noreply.github.com>
2022-04-30 04:22:46 +0200

    files
    open allicat_eco in both html & css

---
+++ Scripts/allicat-list.js
@@ -0,0 +1,11 @@
+/*Navigation bar functionality, adds responsive class when clicking on a button*/
+function navBarFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+/*Headlines functionality*/
